c18644dc831fd491834a84417d76a587b64b6ff6
TakayukiGunji <vizen-ume-12@docomo.ne.jp>
2025-03-07 15:44:52 +0900

    rand

--- PACK-get.php
+++ PACK-get.php
@@ -1,16 +1,20 @@
 <?php
-$pack_1 = rand ( 1, 723);
+
+$min = 1;
+$max = 723;
+
+$pack_1 = rand ( $min, $max);
 $pack_2 = rand ( 1, 723);
 $pack_3 = rand ( 1, 723);
 $pack_4 = rand ( 1, 723);
 $pack_5 = rand ( 1, 723);
 
 ?>
-<img src="images/m<?= $pack_1 ?>.png" alt="ランダムカード">
-<img src="images/m<?= $pack_2 ?>.png" alt="ランダムカード">
-<img src="images/m<?= $pack_3 ?>.png" alt="ランダムカード">
-<img src="images/m<?= $pack_4 ?>.png" alt="ランダムカード">
-<img src="images/m<?= $pack_5 ?>.png" alt="ランダムカード">
+<img src="images/m<?= $pack_1 ?>.png" alt="1枚目">
+<img src="images/m<?= $pack_2 ?>.png" alt="2枚目">
+<img src="images/m<?= $pack_3 ?>.png" alt="3枚目">
+<img src="images/m<?= $pack_4 ?>.png" alt="4枚目">
+<img src="images/m<?= $pack_5 ?>.png" alt="5枚目">
 
 <form action="PACK-get.php" method='post'>
     <input type="submit" name="submit" value="欲張ってもうパックを引く！">
