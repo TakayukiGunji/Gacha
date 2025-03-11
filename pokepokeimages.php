c7d63f247290c5a9bfa73437f1268aebd13fe923
TakayukiGunji <vizen-ume-12@docomo.ne.jp>
2025-03-11 15:31:18 +0900

    20250311last

--- pokepokeimages.php
+++ pokepokeimages.php
@@ -2,16 +2,16 @@
 echo "スクレイピングを開始します...\n";
 
 $save_dir = "images/";
-if (!is_dir($save_dir)) {
-    mkdir ($save_dir, 0777, true);
+if ( !is_dir ( $save_dir ) ) {
+    mkdir ( $save_dir, 0777, true );
 }
 
-$start = 0;
+$start = 1;
 $end = 800;
 
-for ($i = $start; $i <= $end; $i++) {
+for ( $i = $start; $i <= $end; $i++ ) {
     $image_url = "https://img.gamewith.jp/article_tools/pokemon-tcg-pocket/gacha/m$i.png";
-    $image_data = @file_get_contents($image_url);
+    $image_data = @file_get_contents ( $image_url );
 
     if ($image_data === false) {
         echo "画像が見つかりません: $image_url\n";
@@ -19,7 +19,7 @@ for ($i = $start; $i <= $end; $i++) {
     }
 
     $file_name = $save_dir . "m$i.png";
-    file_put_contents($file_name, $image_data);
+    file_put_contents ( $file_name, $image_data );
     echo "保存しました: $file_name\n";
 }
 
