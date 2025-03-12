d21415c652a1674874da3a07ff925d2abc5d7af4
TakayukiGunji <vizen-ume-12@docomo.ne.jp>
2025-03-12 16:56:52 +0900

    20250312last

---
+++ PACK-open-superfunction.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="hue.css">
+
+</head>
+<body>
+    
+<section id="main" >
+    <div id="kyomu">
+        <h1>パックを無料で引き続けるという虚無<br>引き続けたのちに訪れる黙示</h1>
+    </div>
+    
+    <div id="open_button">
+        <form action="PACK-get-superfunction.php" method='post'>
+            <a href="PACK-get-superfunction.php" class="button"><span>1&nbsp;PACK&emsp;0&nbsp;JPY</span></a>
+        </form>
+    </div>
+</section>
+
+
+
+</body>
+</html>
