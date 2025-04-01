b572b8b3a831dff664c3168bbc19b41a6fab99d7
TakayukiGunji <vizen-ume-12@docomo.ne.jp>
2025-04-01 15:32:38 +0900

    20250401

---
+++ Card_insert.sql
@@ -0,0 +1,9 @@
+
+
+-- id INT PRIMARY KEY AUTO_INCREMENT,
+-- name VARCHAR ( 50 ) NOT NULL,
+-- image VARCHAR ( 255 ),
+-- rare INT NOT NULL
+
+while ( )
+    INSERT INTO PokePoke VALUES ( NULL, $name, $image, $rare );
